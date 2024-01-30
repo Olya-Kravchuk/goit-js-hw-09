c55a95754a0505486bd7afda437705a50309f644
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-01-30 13:13:48 +0100

    gggg

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { images } from "./images";
+import { images } from "./img";
 
 const galleryContainer = document.querySelector('.gallery');
 
